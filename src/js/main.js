548c511f0761e71b2c444470c3e054d982f49ac1
Sandra Fernandz <sandraenmunich@gmail.com>
2021-12-06 17:56:14 +0100

    rewrite js function

--- src/js/main.js
+++ src/js/main.js
@@ -5,7 +5,12 @@ console.log('>> Ready :)');
 const cardsElement = document.querySelector('.js-cards');
 
 function getProductHtmlCode() {
-  const htmlCode = '<p>Camiseta 1</p>';
+  let htmlCode = `<article class="card">`;
+  htmlCode += `<img src="./assets/images/node-js.jpg" class="card__img" alt="Camiseta de Node JS">`;
+  htmlCode += `<h3 class="card__title">Node JS desde JS</h3>`;
+  htmlCode += `<p class="card__description">12,00 €</p>`;
+  htmlCode += `<button class="card__btn">Añadir a la cesta</button>`;
+  htmlCode += `</article>`;
   return htmlCode;
 }
 
