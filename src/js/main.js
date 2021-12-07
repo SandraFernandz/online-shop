2ff6fb66ab0bd631abda251cb1a5cb6e8b851899
Sandra Fernandz <sandraenmunich@gmail.com>
2021-12-07 09:25:27 +0100

    parametrise price

--- src/js/main.js
+++ src/js/main.js
@@ -4,16 +4,18 @@ console.log('>> Ready :)');
 
 const cardsElement = document.querySelector('.js-cards');
 
-function getProductHtmlCode() {
+function getProductHtmlCode(name, price) {
+  console.log(name, price);
+  console.log(price, name);
   let htmlCode = `<article class="card">`;
   htmlCode += `<img src="./assets/images/node-js.jpg" class="card__img" alt="Camiseta de Node JS">`;
-  htmlCode += `<h3 class="card__title">Node JS desde JS</h3>`;
-  htmlCode += `<p class="card__description">12,00 €</p>`;
+  htmlCode += `<h3 class="card__title">${name}</h3>`;
+  htmlCode += `<p class="card__description">${price} €</p>`;
   htmlCode += `<button class="card__btn">Añadir a la cesta</button>`;
   htmlCode += `</article>`;
   return htmlCode;
 }
 
-const htmlCode = getProductHtmlCode();
+const htmlCode = getProductHtmlCode('Node JS', 13.2);
 
 cardsElement.innerHTML = htmlCode;
