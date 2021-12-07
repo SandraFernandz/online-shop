f4777c193d0377d627bc1385f6400a86f953f9a7
Sandra Fernandz <sandraenmunich@gmail.com>
2021-12-07 09:47:57 +0100

    secure first function

--- src/js/main.js
+++ src/js/main.js
@@ -4,11 +4,11 @@ console.log('>> Ready :)');
 
 const cardsElement = document.querySelector('.js-cards');
 
-function getProductHtmlCode(name, price, image) {
+function getProductHtmlCode(name, price, imageUrl) {
   console.log(name, price);
   console.log(price, name);
   let htmlCode = `<article class="card">`;
-  htmlCode += `<img src= ${image} class="card__img" alt="Camiseta de Node JS">`;
+  htmlCode += `<img src= ${imageUrl} class="card__img" alt="Camiseta de ${name}">`;
   htmlCode += `<h3 class="card__title">${name}</h3>`;
   htmlCode += `<p class="card__description">${price} €</p>`;
   htmlCode += `<button class="card__btn">Añadir a la cesta</button>`;
@@ -18,7 +18,7 @@ function getProductHtmlCode(name, price, image) {
 
 const htmlCode = getProductHtmlCode(
   'Node JS',
-  13.2,
+  '13,20',
   `./assets/images/node-js.jpg`
 );
 
