a01a0f923aa6c7a58d316548428715a0d2cb432f
Sandra Fernandz <sandraenmunich@gmail.com>
2021-12-07 13:36:41 +0100

    build dinamic item chart

--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,30 @@
 'use strict';
 
 console.log('>> Ready :)');
+// data
 
+const product1Name = 'Node JS';
+const product1Price = '12,50';
+const product1ImageUrl = './assets/images/node-js.jpg';
+//let product1Quantity = 1;
+
+const product2Name = 'JavaScript';
+const product2Price = '13,10';
+const product2ImageUrl = './assets/images/javascript.jpg';
+//let product2Quantity = 1;
+
+const product3Name = 'React';
+const product3Price = '13,20';
+const product3ImageUrl = './assets/images/react.jpg';
+//let product3Quantity = 1;
+
+// bring elements from HTML to JS
 const cardsElement = document.querySelector('.js-cards');
+const cartElement = document.querySelector('.js-cart');
 
 // función que genera camisetas dinámicamente y usando parámetros para personalizar cada card
 
 function getProductHtmlCode(name, price, imageUrl) {
-  console.log(name, price);
-  console.log(price, name);
   let htmlCode = `<article class="card">`;
   htmlCode += `<img src= ${imageUrl} class="card__img" alt="Camiseta de ${name}">`;
   htmlCode += `<h3 class="card__title">${name}</h3>`;
@@ -22,21 +38,21 @@ function getProductHtmlCode(name, price, imageUrl) {
 
 function paintProducts() {
   const product1 = getProductHtmlCode(
-    'Node JS',
-    '13,20',
-    `./assets/images/node-js.jpg`
+    product1Name,
+    product1Price,
+    product1ImageUrl
   );
 
   const product2 = getProductHtmlCode(
-    'JavaScript',
-    '13,50',
-    `./assets/images/javascript.jpg`
+    product2Name,
+    product2Price,
+    product2ImageUrl
   );
 
   const product3 = getProductHtmlCode(
-    'JavaScript',
-    '13,50',
-    `./assets/images/react.jpg`
+    product3Name,
+    product3Price,
+    product3ImageUrl
   );
 
   cardsElement.innerHTML = product1 + product2 + product3;
